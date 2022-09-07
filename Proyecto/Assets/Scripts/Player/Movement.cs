2c472aa95797d32b9166e7434f1858fe06ef54de
feliS317 <felipesalazar@vtr.net>
2022-09-07 18:35:52 -0400

    Eliminado cooperativo
    No es requisito este semestre

--- Proyecto/Assets/Scripts/Player/Movement.cs
+++ Proyecto/Assets/Scripts/Player/Movement.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    //private Vector2 movement = Vector2.zero;
     [SerializeField] private float characterSpeed = 3f;
     private Animator animator;
     private Rigidbody2D rb;
     [Header("Salto")]
     public float jumpForce;
     private bool isGrounded;
-    //private bool jump;
     public Transform groundCheckpoint;
     public LayerMask ground;
 
     // Start is called before the first frame update
-
-    /*public void OnMove(InputAction.CallbackContext context) // Movimiento con InputSystem
-    {
-        movement = context.ReadValue<Vector2>();
-    }
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        jump = context.action.triggered;
-    }*/
-
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -36,11 +24,10 @@ public class Movement : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector2(characterSpeed * Input.GetAxisRaw("Horizontal"), rb.velocity.y);
-        // rb.velocity = new Vector2(characterSpeed * movement.x, rb.velocity.y);
 
         isGrounded = Physics2D.OverlapCircle(groundCheckpoint.position, .2f, ground);
 
-        if(Input.GetKeyDown(KeyCode.T /*jump*/) &&  isGrounded){
+        if(Input.GetKeyDown(KeyCode.T) &&  isGrounded){
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
     }
